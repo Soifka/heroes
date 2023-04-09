746301fe1453e4b6feac29d4e1e88293f2944703
Julia Alekseeva <soifka@gmail.com>
2023-04-09 19:24:50 +0300

    Add superpower migration

---
+++ server/migrations/3-create-superpower.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('superpowers', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                type: Sequelize.STRING
+            },
+            superheroId: {
+                field: 'superhero_id',
+                allowNull: false,
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'superheroes',
+                    key: id
+                },
+                onUpdate: 'cascade',
+                onDelete: 'cascade',
+            },
+            createdAt: {
+                field: 'created_at',
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                field: 'updated_at',
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        })
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('superpowers');
+    }
+}
