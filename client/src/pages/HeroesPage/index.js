a7615ab6018778f5a416341761aff112cfa007f0
Julia Alekseeva <soifka@gmail.com>
2023-04-11 20:45:34 +0300

    Add search by hero nickname

--- client/src/pages/HeroesPage/index.js
+++ client/src/pages/HeroesPage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Hero from '../../components/Hero';
 import { getHeroes } from '../../redux/slices/heroSlice';
@@ -7,6 +7,7 @@ import styles from './HeroesPage.module.css';
 const HeroesPage = () => {
     const { heroes, isLoading, error } = useSelector((state) => state.heroes);
     const dispatch = useDispatch();
+    const [searchHero, setSearchHero] = useState('');
 
     useEffect(() => {
         dispatch(getHeroes());
@@ -20,10 +21,20 @@ const HeroesPage = () => {
         return <div>ERROR</div>
     }
 
-    const heroesCards = heroes.map(hero => <Hero key={hero.id} hero={hero} />)
+    const filteredHeroes = heroes.filter((hero) => 
+        hero.nickname.toLowerCase().includes(searchHero.toLowerCase())
+    )
+
+    const heroesCards = filteredHeroes.map(hero => <Hero key={hero.id} hero={hero} />)
 
     return (
         <div>
+            <input 
+            type="text" 
+            value={searchHero} 
+            onChange={({target: {value}}) => setSearchHero(value)} 
+            placeholder='Search by hero nickname'
+            />
             {heroesCards}
         </div>
     );
