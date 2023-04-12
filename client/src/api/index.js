7e8e56c8000f721545abf1067dfe047e7f59db95
Julia Alekseeva <soifka@gmail.com>
2023-04-12 19:18:24 +0300

    AddSuperpower done

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -9,3 +9,5 @@ export const getHeroes = async () => httpClient.get('/superheroes');
 export const deleteHero = async (superheroId) => httpClient.delete(`/superheroes/${superheroId}`);
 
 export const deletePower = async (superheroId, powerId) => httpClient.delete(`/superheroes/${superheroId}/superpowers/${powerId}`);
+
+export const addPower = async (superheroId, superpowers) => httpClient.post(`/superheroes/${superheroId}/superpowers`, {superpowers});
--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -3,16 +3,26 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from './Hero.module.css';
-import { deleteHero, deletePower } from '../../api';
+import { deleteHero, deletePower, addPower } from '../../api';
 import { getHeroes } from '../../redux/slices/heroSlice';
 import { useDispatch } from 'react-redux';
 import Modal from 'react-modal';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 Modal.setAppElement('#root');
+
+const validationPowerSchema = yup.object().shape({
+    powerName: yup.string()
+    .trim()
+    .min(3, 'Superpower name should be longer than 3 symbols')
+    .required('Superpower name is required')
+})
 
 
 const Hero = ({hero}) => {
     const [modalOpen, setModalOpen] = useState(false);
+    const [modalAddPowerOpen, setModalAddPowerOpen] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -34,6 +44,17 @@ const Hero = ({hero}) => {
         await deletePower(hero.id, powerId);
         dispatch(getHeroes());
         setModalOpen(false);
+    }
+
+    const handleAddPowerSubmit = async (values, {resetForm}) => {
+        try {
+            await addPower(hero.id, [values.powerName]);
+            dispatch(getHeroes());
+            setModalAddPowerOpen(false);
+            resetForm();
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     return (
@@ -89,6 +110,44 @@ const Hero = ({hero}) => {
                 <p>Are you sure you want to delete {hero.nickname}?</p>
                 <button onClick={deleteHandler}>Yes</button>
                 <button onClick={() => setModalOpen(false)}>No</button>
+            </Modal>
+
+            <button onClick={() => setModalAddPowerOpen(true)}>Add superpower</button>
+
+            <Modal
+                isOpen={modalAddPowerOpen}
+                onRequestClose={() => setModalAddPowerOpen(false)}
+                contentLabel="Add Superpower Modal"
+                style={{
+                    content: {
+                        top: '50%',
+                        left: '50%',
+                        right: 'auto',
+                        bottom: 'auto',
+                        marginRight: '-50%',
+                        transform: 'translate(-50%, -50%)',
+                    }
+                }}
+            >
+                <h2>Add superpower</h2>
+                <Formik
+                    initialValues={{powerName: ''}}
+                    validationSchema={validationPowerSchema}
+                    onSubmit={handleAddPowerSubmit}
+                >
+                    {(props) => (
+                        <Form>
+                            <label>
+                                Superpower name
+                                <Field type="text" name="powerName" />
+                                <ErrorMessage name="powerName" />
+                            </label>
+
+                            <button type="submit">Confirm</button>
+                            <button type="button" onClick={() => setModalAddPowerOpen(false)}>Cancel</button>
+                        </Form>
+                    )}
+                </Formik>
             </Modal>
         </article>
     );
